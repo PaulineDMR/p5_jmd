ac5bb23b465e17ed4dbadf0ea3b3bebd995b6eea
PaulineDMR <pauline.desmares@gmail.com>
2018-06-14 12:39:46 +0200

    Creta controller and templates to display portfolio page

---
+++ src/controllers/PortfolioController.php
@@ -0,0 +1,39 @@
+<?php 
+
+namespace jmd\controllers;
+
+class PortfolioController extends Controller {
+	
+
+	public function groupPaintings($paintings, $group) {
+		$paintingGroup = [];
+
+		$ix = $group-1;
+
+		for ($i = $ix; $i < count($paintings); $i = $i + 3) {
+			$paintingGroup[] = $paintings[$i];
+		}
+
+		return $paintingGroup;
+	}
+
+	public function render() {
+		$paintings = $this->paintingManager->GetAllPaintings();
+
+		$group1 = $this->groupPaintings($paintings, $group = 1);
+		$group2 = $this->groupPaintings($paintings, $group = 2);
+		$group3 = $this->groupPaintings($paintings, $group = 3);
+
+		$twig = \jmd\models\Twig::initTwig();
+
+		echo $twig->render("portfolioContent.twig",
+			[
+			"paintings" => $paintings,
+			"col1" => $group1,
+			"col2" => $group2,
+			"col3" => $group3
+			]);
+
+	}
+
+}
